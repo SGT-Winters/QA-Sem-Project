a96ab93008e6bc641bd271a244a4b89cfcdc08f0
Rafay Hassan <rafayhassan02@gmail.com>
2024-11-23 20:52:03 -0500

    upate

---
+++ com/example/Calculator_Web_App/CalculatorWebAppApplication.java
@@ -0,0 +1,15 @@
+package com.example.Calculator_Web_App;
+
+import jdk.internal.misc.InnocuousThread;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWebAppApplication {
+
+	public static void main(String[] args) {
+		InnocuousThread SpringApplication;
+		SpringApplication.run(CalculatorWebAppApplication.class, args);
+	}
+
+}
